41606a7ce5ee92efddd75375be4cd33dde802686
Josseline Perdomo <jperdomo@mahisoft.com>
2019-04-13 22:55:16 -0400

    Initial Spring Boot config

---
+++ src/main/java/org/alumnusb/easypay/EasypayServiceApplication.java
@@ -0,0 +1,15 @@
+package org.alumnusb.easypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@SuppressWarnings("PMD.UseUtilityClass")
+public class EasypayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasypayServiceApplication.class, args);
+	}
+}
