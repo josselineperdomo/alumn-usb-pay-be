3f85e0213b1c114ae8f4695114977523de5d4a0e
Yerson Roa <yroa@mahisoft.com>
2019-04-16 20:09:47 -0400

    Removed unused imports.

--- src/main/java/org/alumnusb/easypay/model/BeneficiaryType.java
+++ src/main/java/org/alumnusb/easypay/model/BeneficiaryType.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,12 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Data
 @Builder
